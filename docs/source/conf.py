b5dfcb71e119b6c87fc1782009b756567cc0a76d
CristianTacoronteRivero <tacoronteriverocristian@gmail.com>
2024-10-29 10:50:35 +0000

    feat: modificar tema

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,5 +40,5 @@ language = "es"
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "furo"
+html_theme = "bizstyle"
 html_static_path = ["_static"]
