2ded20d9776780d52afb64f92a50cf0c8735c44c
CristianTacoronteRivero <tacoronteriverocristian@gmail.com>
2024-11-04 09:21:28 +0000

    docs: mejorar presentacion de la documentacion

--- ctrutils/template/email_sender.py
+++ ctrutils/template/email_sender.py
@@ -1,33 +1,9 @@
 """
-Este script proporciona la clase `EmailSender` para enviar correos electronicos
-utilizando el servidor SMTP de Gmail. Permite especificar destinatarios, incluir
-archivos adjuntos y agregar direcciones de copia oculta (BCC).
+Modulo para enviar correos electronicos utilizando el servidor SMTP de Gmail.
 
-### Descripcion
-La clase `EmailSender` facilita el envio de correos electronicos utilizando el
-protocolo SMTP de Gmail. Incluye la capacidad de adjuntar múltiples archivos y
-agregar destinatarios en copia oculta para una mayor flexibilidad.
-
-### Parámetros del constructor
-- **user_gmail** (str): Direccion de correo de Gmail utilizada para el envio.
-- **app_password** (str): Contraseña de la aplicacion generada en la configuracion
-  de Gmail. Se requiere habilitar el acceso de aplicaciones menos seguras o usar
-  la autenticacion de dos factores y generar una contraseña especifica para aplicaciones.
-
-### Ejemplo de uso
-```python
-# Crear una instancia de EmailSender
-email_sender = EmailSender("myuser@gmail.com", "mypassword")
-
-# Configurar los detalles del correo
-to_address = "recipient@example.com"
-bcc_addresses = "hidden@example.com"
-subject = "Asunto del correo"
-body = "Este es el cuerpo del correo. Adjunto un archivo."
-attachments = "documento1.pdf,imagen2.jpg"
-
-# Enviar el correo
-email_sender.send_email(to_address, subject, body, attachments, bcc_addresses)
+Este modulo proporciona la clase `EmailSender` que facilita el envio de correos
+electronicos con opciones para destinatarios, archivos adjuntos y direcciones en
+copia oculta (BCC).
 """
 
 import os
@@ -43,22 +19,42 @@ class EmailSender:
     """
     Clase para enviar correos electronicos utilizando el servidor SMTP de Gmail.
 
-    :param user_gmail: Direccion de correo de Gmail.
+    :param user_gmail: Direccion de correo de Gmail utilizada para el envio.
     :type user_gmail: str
-    :param app_password: Contraseña de la aplicacion generada en Gmail.
+    :param app_password: Contraseña de la aplicacion generada en la configuracion
+                         de Gmail. Se requiere habilitar el acceso de aplicaciones
+                         menos seguras o usar la autenticacion de dos factores y
+                         generar una contraseña especifica para aplicaciones.
     :type app_password: str
+
+    **Ejemplo de uso**:
+
+    .. code-block:: python
+
+        # Crear una instancia de EmailSender
+        email_sender = EmailSender("myuser@gmail.com", "mypassword")
+
+        # Configurar los detalles del correo
+        to_address = "recipient@example.com"
+        bcc_addresses = "hidden@example.com"
+        subject = "Asunto del correo"
+        body = "Este es el cuerpo del correo. Adjunto un archivo."
+        attachments = "documento1.pdf,imagen2.jpg"
+
+        # Enviar el correo
+        email_sender.send_email(to_address, subject, body, attachments, bcc_addresses)
     """
 
     def __init__(self, user_gmail: str, app_password: str) -> None:
         """
-        Constructor de la clase EmailSender.
+        Inicializa la clase EmailSender con las credenciales proporcionadas.
 
         :param user_gmail: Direccion de correo electronico de Gmail.
         :type user_gmail: str
         :param app_password: Contraseña de la aplicacion generada en la configuracion
-          de Gmail. Se requiere habilitar el acceso de aplicaciones menos seguras o
-          usar la autenticacion de dos factores y generar una contraseña especifica
-          para aplicaciones.
+                             de Gmail. Se requiere habilitar el acceso de aplicaciones
+                             menos seguras o usar la autenticacion de dos factores y
+                             generar una contraseña especifica para aplicaciones.
         :type app_password: str
         """
         self.user_gmail = user_gmail
@@ -91,9 +87,9 @@ class EmailSender:
         # Crear el mensaje
         msg = MIMEMultipart()
         msg["From"] = self.user_gmail
-        msg["To"] = ", ".join([to_address.replace(" ", "")])
+        msg["To"] = to_address
         if bcc_addresses:
-            msg["Bcc"] = ", ".join([bcc_addresses.replace(" ", "")])
+            msg["Bcc"] = bcc_addresses
         msg["Subject"] = subject
 
         # Agregar el cuerpo del mensaje
@@ -101,7 +97,7 @@ class EmailSender:
 
         # Adjuntar archivos si se especifican
         if attachments:
-            attachments_list = attachments.replace(" ", "").split(",")
+            attachments_list = attachments.split(",")
             for file in attachments_list:
                 attachment = MIMEBase("application", "octet-stream")
                 with open(file, "rb") as attachment_file:
@@ -121,11 +117,15 @@ class EmailSender:
 
 
 if __name__ == "__main__":
+    # Instanciar la clase para enviar correos
     email_sender = EmailSender("myuser@gmail.com", "mypassword")
+
+    # Establecer variables para el parametro de envio
     to_address = "ctacoronte@itccanarias.org"
     bcc_addresses = "itc.eerr.info@gmail.com"
     subject = "Prueba de envio de correo"
     body = "Este es el cuerpo del correo. Adjunto un archivo docx. Aqui hay correos en CCO."
     attachments = "file1.docx,file2.pdf"
 
+    # Enviar el correo
     email_sender.send_email(to_address, subject, body, attachments, bcc_addresses)
